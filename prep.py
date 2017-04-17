b1ce8eb0936f5b9a080588d2228629fe21cbcd91
Mipanox <vcubewca@gmail.com>
2017-04-16 20:25:51 -0700

    simple DFT tests for Airy disk

---
+++ prep.py
@@ -0,0 +1,7 @@
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+import astropy.units as u
+from astropy.utils.data import get_readable_fileobj
+from astropy.io import fits
