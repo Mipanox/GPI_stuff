68659e106837b765095f4b184fe3fa1474fed9bc
Mipanox <vcubewca@gmail.com>
2017-04-17 12:44:52 -0700

    checkpoint

--- prep.py
+++ prep.py
@@ -6,3 +6,10 @@ import astropy.units as u
 from astropy.utils.data import get_readable_fileobj
 from astropy.io import fits
 from scipy.ndimage.interpolation import shift
+
+plt.rcParams['axes.titlesize'] = 20
+plt.rcParams['axes.labelsize'] = 20
+plt.rcParams['xtick.labelsize'] = 18
+plt.rcParams['ytick.labelsize'] = 18
+#plt.rcParams['legend.fontsize'] = 14
+#plt.rc('text', usetex=True)
